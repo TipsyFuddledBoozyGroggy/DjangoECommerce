283586cb22200344588090c1249cb3021f7775f6
Jamie Olsen <jwolsen@mmm.com>
2024-12-07 11:47:43 -0700

    init commit

---
+++ ecommerce/frontend/src/App.js
@@ -0,0 +1,25 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes instead of Route
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-5">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
