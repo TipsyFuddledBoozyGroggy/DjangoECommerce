1ce953f447d6a540ef1c52add46fef5fab117e1d
TipsyFuddledBoozyGroggy <jamiewolsen@gmail.com>
2025-01-01 19:36:44 -0700

    updated to use jwt

--- ecommerce/backend/base/urls.py
+++ ecommerce/backend/base/urls.py
@@ -3,8 +3,10 @@
 from django.urls import path
 from . import views
 
+
 #list of URLs and views to trigger
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>', views.getProduct, name="product"),
--- ecommerce/backend/base/views.py
+++ ecommerce/backend/base/views.py
@@ -4,7 +4,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     token['message'] = 'Hello World!'
+
+
+    #     return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
